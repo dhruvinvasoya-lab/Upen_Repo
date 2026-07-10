15d624b121bda0f78940a529c8af6df53ebba543
QA Demo <upendra.goswami@testgrid.io>
2026-07-10 13:06:02 +0000

    Test Case Updated

--- compile-java-executable/199/testcases/Recordplay.java
+++ compile-java-executable/199/testcases/Recordplay.java
@@ -21,22 +21,23 @@ public class recordplay {
 	@Test(retryAnalyzer = RetryFailedTestCases.class)
 	public void recordplay() {
 		tg.openDevice();
-				tg.wait("ele_homebuttonImageButton1783682916904", ComparisonType.IS_VISIBLE);
-				tg.click("ele_homebuttonImageButton1783682916904", 1);
-				tg.wait("ele_SearchGoogleortypeURLEditText1783682923866", ComparisonType.IS_VISIBLE);
-				tg.click("ele_SearchGoogleortypeURLEditText1783682923866", 1);
-				tg.wait("ele_SearchGoogleortypeURLEditText1783682932116", ComparisonType.IS_VISIBLE);
-				tg.click("ele_SearchGoogleortypeURLEditText1783682932116", 1);
-				tg.wait("ele_SearchGoogleortypeURLEditText1783682932116", ComparisonType.IS_VISIBLE);
-				tg.type("ele_SearchGoogleortypeURLEditText1783682932116", "192.168.88.17:80/Demo", false);
+		tg.wait("ele_homebuttonImageButton1783682916904", ComparisonType.IS_VISIBLE);
+		tg.click("ele_homebuttonImageButton1783682916904", 1);
+		tg.wait("ele_SearchGoogleortypeURLEditText1783682923866", ComparisonType.IS_VISIBLE);
+		tg.click("ele_SearchGoogleortypeURLEditText1783682923866", 1);
+		tg.wait("ele_SearchGoogleortypeURLEditText1783682932116", ComparisonType.IS_VISIBLE);
+		tg.click("ele_SearchGoogleortypeURLEditText1783682932116", 1);
+		tg.wait("ele_SearchGoogleortypeURLEditText1783682932116", ComparisonType.IS_VISIBLE);
+		tg.type("ele_SearchGoogleortypeURLEditText1783682932116", "192.168.88.17:80/Demo", false);
 		START_CUSTOM_SCRIPT;
 		driver.findElement(By.xpath("//android.widget.TextView[@resource-id='com.android.chrome:id/line_1']")).click();
 		END_CUSTOM_SCRIPT;
-				tg.wait(10);
-				tg.wait("ele_TogglenavigationButton1783683156908", ComparisonType.IS_VISIBLE);
-				tg.click("ele_TogglenavigationButton1783683156908", 1);
-				tg.wait("ele_HOMETextView1783683165348", ComparisonType.IS_VISIBLE);
-				tg.click("ele_HOMETextView1783683165348", 1);
+		tg.wait(10);
+		tg.wait("ele_TogglenavigationButton1783683156908", ComparisonType.IS_VISIBLE);
+		tg.click("ele_TogglenavigationButton1783683156908", 1);
+		tg.wait("ele_HOMETextView1783683165348", ComparisonType.IS_VISIBLE);
+		tg.click("ele_HOMETextView1783683165348", 1);
+		tg.wait(5);
 		tg.close();
 	}
 }
