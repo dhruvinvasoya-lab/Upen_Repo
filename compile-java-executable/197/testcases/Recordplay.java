5afbb340a0a0d81eae5cf122ca2bae36d542f7fa
Upendra qainofc1 <upendra.goswami+7@testgrid.io>
2026-07-15 08:57:49 +0000

    tc

--- compile-java-executable/197/testcases/Recordplay.java
+++ compile-java-executable/197/testcases/Recordplay.java
@@ -21,15 +21,24 @@ public class recordplay {
 	@Test(retryAnalyzer = RetryFailedTestCases.class)
 	public void recordplay() {
 		tg.openDevice();
-				tg.wait("ele_TabBarItemTitleTextField1783682569971", ComparisonType.IS_VISIBLE);
-				tg.click("ele_TabBarItemTitleTextField1783682569971", 1);
-				tg.wait("ele_URLTextField1783682576631", ComparisonType.IS_VISIBLE);
-				tg.click("ele_URLTextField1783682576631", 1);
-				tg.wait("ele_URLTextField1783682576631", ComparisonType.IS_VISIBLE);
-				tg.type("ele_URLTextField1783682576631", "192.168.88.17:80/Demo", true);
-				tg.wait("ele_GoButton1783682589558", ComparisonType.IS_VISIBLE);
-				tg.click("ele_GoButton1783682589558", 1);
-				tg.wait(2);
+		tg.wait("ele_TabBarItemTitleTextField1783682569971", ComparisonType.IS_VISIBLE);
+		tg.click("ele_TabBarItemTitleTextField1783682569971", 1);
+		tg.wait("ele_URLTextField1783682576631", ComparisonType.IS_VISIBLE);
+		tg.click("ele_URLTextField1783682576631", 1);
+		tg.wait("ele_URLTextField1783682576631", ComparisonType.IS_VISIBLE);
+		tg.type("ele_URLTextField1783682576631", "192.168.88.17:80/Demo", true);
+		tg.wait("ele_GoButton1783682589558", ComparisonType.IS_VISIBLE);
+		tg.click("ele_GoButton1783682589558", 1);
+		tg.wait(2);
+		tg.wait("ele_TabBarItemTitleTextField1783682569971", ComparisonType.IS_VISIBLE);
+		tg.click("ele_TabBarItemTitleTextField1783682569971", 1);
+		tg.wait("ele_URLTextField1783682576631", ComparisonType.IS_VISIBLE);
+		tg.click("ele_URLTextField1783682576631", 1);
+		tg.wait("ele_URLTextField1783682576631", ComparisonType.IS_VISIBLE);
+		tg.type("ele_URLTextField1783682576631", "192.168.88.17:80/Demo", true);
+		tg.wait("ele_GoButton1783682589558", ComparisonType.IS_VISIBLE);
+		tg.click("ele_GoButton1783682589558", 1);
+		tg.wait(2);
 		tg.close();
 	}
 }
